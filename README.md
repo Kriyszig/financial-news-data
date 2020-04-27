f60d13b5a205119d3e8812ad426b8b57ffad8c5b
Kriyszig <lelouch.cpp@gmail.com>
2020-04-27 23:16:08 +0530

    [Refactor] Refactored code and added the DataFrame as parquet
    * Refactored code to explain itself better
    * Still self documented
    * Added parquet file in another branch for direct download

--- README.md
+++ README.md
@@ -1,3 +1,55 @@
-# financial-news-data
+# Reuters Financial Dataset as a structured DataFrame
 
-A python script to arrange the data from [financial-news-dataset](https://github.com/Danbo3004/financial-news-dataset) into a CSV file
+[Reuters Financial Dataset](https://github.com/Danbo3004/financial-news-dataset) is a large collection of Financial News Article scraped from Reuters website.
+Originally used for the paper [Using Structured Events to Predict Stock Price Movement:An Empirical Investigation - Ding et al.(2014)](http://emnlp2014.org/papers/pdf/EMNLP2014148.pdf)
+this set of unstructured data is a powerful warehouse of historic Financial Data. This script provides a way of arranging the huge corpus of information into
+a [Pandas'](https://pandas.pydata.org/) efficient data structure [DataFrame](https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.html)  
+
+Originally, this repository consisted of badly written Python script which was monolitic and cryptic. This refactor breaks the code down into smaller functions and
+comes equipped with a function to create the DataFrame.
+
+### Usage
+
+The build depends on the following libraries:
+* pandas
+* pyarrow or fastparquet - Pandas optional dependency to read and write DataFrame to parquet format
+
+To generate the parquet file yourself, please run the following commands:
+
+```bash
+git clone https://github.com/Kriyszig/financial-news-data.git
+cd financial-news-data
+python3 main.py
+```
+If you are interested in the prebuilt dataset, please visit the [parquet branch](https://github.com/Kriyszig/financial-news-data/tree/parquet)  
+To do it from command line, run
+
+```bash
+git clone --single-branch --branch parquet https://github.com/Kriyszig/financial-news-data.git
+```
+
+The `financial-data.parquet.gzip` is the file that contains the dataset. To create a DataFrame out of this file, please use the code snippet below:
+
+```python
+import pandas as pd
+df = pd.read_parquet('df.parquet.gzip')
+```
+And you are all set to start manipulating df to suit your needs  
+
+### Dataset
+
+The Dataset has the following columns:
+
+| Columns     | Type                               |
+|-------------|------------------------------------|
+| Headline    | string                             |
+| Journalists | list\<string> (Can be empty)       |
+| Date        | Unix Style Date                    |
+| Link        | Original Reuters article link      |
+| Article     | The complete report (Can be empty) |
+
+**Note**: 
+* Journalists can be an empty list if the original dataset had the field empty
+* Article can be an empty string in case only the headline was reported in th original dataset
+
+In case you run into any troubles, please feel free to open an issue and I'll look into it as soon as possible.
---
+++ main.py
@@ -0,0 +1,115 @@
+import os
+import pandas as pd
+from functools import reduce
+from typing import Union
+
+dataset_path: str = './ReutersNews106521';
+
+def validator(file_path: str) -> Union[list, bool]:
+  with open(file_path, 'r') as f:
+    lines: list = f.readlines()
+    # The file doesn't contain any information
+    # and hence need not be processed
+    if(len(lines) < 4):
+      return False
+    # The file may contain useful headline
+    # but no elaborate description
+    elif(len(lines) == 4):
+      if(len(lines[0].strip('\n').strip()) > 2):
+        return lines
+      else:
+        # If there is no headline in first line
+        # there is no need to process it
+        return False
+    else:
+      return lines if reduce(lambda  a, b: a + b, map(lambda x: 1 if x.startswith('--') else 0, lines[0:4])) == 4 and reduce(lambda a, b: a + b, map(lambda x: len(x.strip('\n').strip()), lines[4:7])) == 0 else False
+
+def parserecord(data: list) -> list:
+  result = []
+  # Parsing the metadata
+  # This constitutes the first 4 lines
+  # -- Headline
+  # -- Journalists
+  # -- Data
+  # -- Link
+  for i, ele in enumerate(data[0:4]):
+    clean_ele = ele.lstrip('- ').rstrip(' \n')
+    if(i == 1):
+      # Multiple journalists are mentioned in the form
+      # -- By X, Y and Z
+      authors = clean_ele.lstrip(' By').split('and')
+      if(len(authors) > 1):
+        authors.extend(authors[0].split(','))
+        authors.pop(0)
+      result.append([i.strip() for i in authors])
+    else:
+      result.append(clean_ele)
+
+  # Parsing the article body
+  content: str = ''
+  if(len(data) > 4):
+    for i in data[4:]:
+      content += i.rstrip('\n').strip()
+  result.append(content)
+
+  return result
+
+def constructdf() -> pd.DataFrame:
+  files = os.listdir(dataset_path)
+  cnt_empty = 0
+  final_df = pd.DataFrame()
+
+  for i in files:
+    if(i.endswith('.DS_Store')):
+      continue
+    nested_files = os.listdir(dataset_path + '/' + i)
+    for j in nested_files:
+      if(j.endswith('.gz') or j.endswith('.DS') or j.endswith('.vscode')):
+        continue
+
+      return_code: Union[list, bool] = validator(dataset_path + '/' + i + '/' + j)
+      if(return_code == False):
+        continue
+      else:
+        parsedrecord = parserecord(return_code)
+        final_df = pd.concat([final_df, pd.DataFrame([parsedrecord])], ignore_index = True)
+
+  return final_df
+
+# Debug
+# Check if the validator works correctly by
+# going through the entire dataset
+if(__name__ == '__main__'):
+  files = os.listdir(dataset_path)
+  cnt_empty = 0
+  print('Validating unstructured data....')
+  for i in files:
+    if(i.endswith('.DS_Store')):
+      continue
+    nested_files = os.listdir(dataset_path + '/' + i)
+    for j in nested_files:
+      if(j.endswith('.gz') or j.endswith('.DS') or j.endswith('.vscode')):
+        continue
+
+      return_code: Union[list, bool] = validator(dataset_path + '/' + i + '/' + j)
+      if(return_code == False):
+        cnt_empty += 1
+      else:
+        parsedrecordlen = len(parserecord(return_code))
+        # Each record should have 5 columns
+        # No more, no less
+        # Headline, authors, date, link, body
+        if(parsedrecordlen != 5):
+          raise Exception("ParserError\nRecord parsing expects list of length 5 but found {} at {}/{}/{}".format(parsedrecordlen, dataset_path, i, j))
+
+  if(cnt_empty != 25):
+    raise Exception("ValidatorError\nEmpty record count off. Expected 25 but found {}".format(cnt_empty))
+
+  print('Constructing DataFrame for the financial data....')
+  financialdf = constructdf()
+  print('Finished constructing DataFrame of shape: ', financialdf.shape)
+
+  print('Renaming columns....')
+  financialdf.columns = ['Headline', 'Journalists', 'Date', 'Link', 'Article']
+  print('Saving the DataFrame as a gzipped parquet....')
+  financialdf.to_parquet('financial_data.parquet.gzip',compression='gzip')
